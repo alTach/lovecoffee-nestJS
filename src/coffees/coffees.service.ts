f2d530b4a4e92bfe48b99a58cb094072bfdd9ec2
taliev <taliev@luxoft.com>
2020-12-24 23:23:05 +0300

    - add entity; - coffee controller and service;

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,44 @@
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entites';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [{
+    id: 1,
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  }];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    throw 'a random message'
+    const coffees = this.coffees.find(item => item.id === +id);
+    if (!coffees) {
+      return new NotFoundException(`Coffee ${id} not found`);
+    }
+    return coffees;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      const existingCoffeeIndex = this.findAll().findIndex(coffee => coffee.id === +id);
+      // this.coffees[existingCoffeeIndex] = existingCoffee;
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.findAll().findIndex(coffee => coffee.id === +id);
+    if (coffeeIndex) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
